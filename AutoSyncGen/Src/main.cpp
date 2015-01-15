45dfd18c81112d4e908516cd7e65d13742654716
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-01-15 10:50:34 +0100

    progress

--- AutoSyncGen/Src/main.cpp
+++ AutoSyncGen/Src/main.cpp
@@ -85,8 +85,173 @@ template<> struct LifetimeManager<TestEnemy>
 	}
 };
 
+using PacketType = signed char;
+
+enum PacketCtoS : PacketType
+{
+	ConnectionRequest = 0,
+};
+
+enum PacketStoC : PacketType
+{
+	ConnectionAccept = 0,
+	ConnectionDecline = 1,
+};
+
+inline void fillPacket(sf::Packet&)
+{
+
+}
+template<typename T, typename... TArgs> inline void fillPacket(sf::Packet& mPacket, T&& mArg, TArgs&&... mArgs)
+{
+	mPacket << mArg;
+	fillPacket(mPacket, ssvu::fwd<TArgs>(mArgs)...);
+}
+
+template<typename TPacketType> class PacketHandler
+{
+	private:
+		using HandlerFunc = ssvu::Func<void(sf::Packet&)>;
+		std::map<TPacketType, HandlerFunc> funcs;
+		sf::Packet& buffer;
+
+	public:
+		inline PacketHandler(sf::Packet& mBuffer) : buffer{mBuffer} 
+		{ 
+
+		}
+
+		inline void handle(sf::Packet& mPacket)
+		{
+			PacketType type{-1};
+
+			try
+			{
+				mPacket >> type;
+
+				auto itr(funcs.find(type));
+				if(itr == std::end(funcs))
+				{
+					ssvu::lo("PacketHandler") << "Can't handle packet of type: " << type << std::endl;
+					return;
+				}
+
+				itr->second(mPacket);
+			}
+			catch(std::exception& mEx)
+			{
+				ssvu::lo("PacketHandler") << "Exception during packet handling: (" << type << ")\n" << mEx.what() << std::endl;
+			}
+			catch(...)
+			{
+				ssvu::lo("PacketHandler") << "Unknown exception during packet handling: (" << type << ")\n";
+			}
+		}
+
+		auto& operator[](TPacketType mType) { return funcs[mType]; }
+};
+
+class ConsoleSessionController;
+
+template<typename TSPT, typename TRPT> class SessionHost
+{
+	public:	
+		using SPT = TSPT;
+		using RPT = TRPT;
+
+	private:
+		std::string name;
+		syn::IpAddress ip;
+		syn::Port port;
+		sf::UdpSocket socket;	
+
+		inline void tryBindSocket()
+		{
+			if(socket.bind(port) != sf::Socket::Done)
+			{
+				throw std::runtime_error("Error binding socket");
+			}
+			else
+			{
+				debugLo() << "Socket successfully bound to port " + ssvu::toStr(port) + "\n";
+			}
+		}
+
+		ssvu::SizeT packetNext;
+
+	protected:
+		sf::Packet buffer;
+		std::future<void> hostFuture;
+		PacketHandler<RPT> handler;
+
+		template<SPT TType, typename... TArgs> inline void mkPacket(TArgs&&... mArgs)
+		{
+			buffer.clear();
+			buffer << static_cast<PacketType>(TType);
+			fillPacket(buffer, ssvu::fwd<TArgs>(mArgs)...);
+		}	
+
+	public:
+		inline SessionHost(std::string mName, syn::Port mPort) : name{mName}, ip{syn::IpAddress::getLocalAddress()}, port{mPort}, handler{buffer}
+		{
+			socket.setBlocking(true);
+		}
+
+		inline const auto& getName() const noexcept { return name; }
+		inline const auto& getIp() const noexcept { return ip; }
+		inline const auto& getPort() const noexcept { return port; }
+
+		inline auto debugLo() -> decltype(ssvu::lo(name))
+		{
+			// TODO: ssvu::getNullLogStream(); for fake logs
+			return ssvu::lo(name);
+		}
+};
+
+class SessionServer : public SessionHost<PacketStoC, PacketCtoS>
+{
+	private:
+
+	public:
+		inline SessionServer(syn::Port mPort) : SessionHost{"Server", mPort} 
+		{
+
+		}
+};
+
+class SessionClient : public SessionHost<PacketCtoS, PacketStoC>
+{
+	public:
+		static constexpr int nullClientID{-1};
+
+	private:
+		syn::IpAddress serverIp;
+		syn::Port serverPort;
+
+		// Assigned from server after connection is accepted
+		int clientID{nullClientID};
+
+		inline void sendConnectionRequest()
+		{
+			mkPacket<SPT::ConnectionRequest>();
+		}
+		
+	public:
+		inline SessionClient(syn::Port mPort, syn::IpAddress mServerIp, syn::Port mServerPort) 
+			: SessionHost{"Client", mPort}, serverIp{mServerIp}, serverPort{mServerPort}
+		{
+			handler[RPT::ConnectionAccept] = [this](sf::Packet& mP)
+			{
+
+			};
+		}
+
+};
+
 class Session
 {
+	friend class ConsoleSessionController;
+
 	private:
 		enum class Role{Server, Client};
 
@@ -95,28 +260,105 @@ class Session
 		syn::Port port;
 		sf::UdpSocket socket;
 
+		std::future<void> socketFuture;
+
+		static constexpr ssvu::SizeT bufferSize{2048};
+		std::array<char, 2048> buffer;
+
+		inline void tryBindSocket()
+		{
+			if(socket.bind(port) != sf::Socket::Done)
+			{
+				throw std::runtime_error("Error binding socket");
+			}
+			else
+			{
+				ssvu::lo() << "Socket successfully bound to port " + ssvu::toStr(port) + "\n";
+			}
+		}
+
+		inline void serverFutureImpl()
+		{
+			syn::IpAddress senderIp;
+			syn::Port senderPort;
+			ssvu::SizeT receivedSize;
+
+			while(true)
+			{
+				if(socket.receive(buffer.data(), bufferSize, receivedSize, senderIp, senderPort) != sf::Socket::Done)
+				{
+				    ssvu::lo("Server") 	<< "Socket receive error: \n" 
+				  						<< "	Sender: " << senderIp << ":" << senderPort << "\n";
+
+				  	continue;
+				}
+
+				ssvu::lo("Server") 	<< "Received " << receivedSize << " bytes from: \n" 
+				  					<< "	Sender: " << senderIp << ":" << senderPort << "\n";
+			}
+		}
+
+		inline void clientFutureImpl()
+		{
+			while(true)
+			{
+
+			}
+		}
+
+		inline void startSocketFuture()
+		{
+			if(role == Role::Server)
+			{
+				socketFuture = std::async(std::launch::async,  [this]{ serverFutureImpl(); });
+			}
+			else
+			{
+				socketFuture = std::async(std::launch::async,  [this]{ clientFutureImpl(); });
+			}
+		}
+
+	
+
+	public:
+		inline Session()
+		{
+			socket.setBlocking(true);
+		}
+
+		inline void join()
+		{
+			socketFuture.get();
+		}	
+};
+
+class ConsoleSessionController
+{
+	private:
+		Session& session;
+
 		inline void selectRole()
 		{
 			while(true)
 			{
-				int choice{0};
+				int choice{-1};
 
 				ssvu::lo() 	<< "Select: \n"
-							<< "    1. Server\n" 
-							<< "    2. Client\n"  
+							<< "    0. Server\n" 
+							<< "    1. Client\n"  
 							<< std::endl;
 
 				std::cin >> choice;
 
-				if(choice == 1)
+				if(choice == 0)
 				{
-					role = Role::Server;
+					session.role = Session::Role::Server;
 					ssvu::lo() << "Server selected\n";
 					break;
 				}
-				else if(choice == 2)
+				else if(choice == 1)
 				{
-					role = Role::Client;
+					session.role = Session::Role::Client;
 					ssvu::lo() << "Client selected\n";
 					break;
 				}
@@ -128,7 +370,28 @@ class Session
 			}
 		}
 
-		inline void selectData()
+		inline void selectServer()
+		{
+			while(true)
+			{
+				selectPort();
+
+				break;
+			}
+		}
+
+		inline void selectClient()
+		{
+			while(true)
+			{
+				selectIP();
+				selectPort();
+
+				break;
+			}
+		}
+
+		inline void selectIP()
 		{
 			while(true)
 			{
@@ -136,29 +399,47 @@ class Session
 				// TODO: display "sender" or "receiver"
 
 				ssvu::lo() << "Insert ip address: \n";
-				std::cin >> ip;
+				std::cin >> session.ip;
+
+				break;
+			}
+		}
+
+		inline void selectPort()
+		{
+			while(true)
+			{
+				// TODO: check validity
+				// TODO: display "sender" or "receiver"
 
 				ssvu::lo() << "Insert port: \n";
-				std::cin >> port;
+				std::cin >> session.port;
 
 				break;
 			}
 		}
 
 	public:
+		inline ConsoleSessionController(Session& mSession) noexcept : session{mSession}
+		{
+
+		}
+
 		inline void start()
 		{
 			selectRole();
-			selectData();
-
-			if(socket.bind(port) != sf::Socket::Done)
+			
+			if(session.role == Session::Role::Server)
 			{
-				throw std::runtime_error("Error binding socket");
+				selectServer();
 			}
 			else
 			{
-				ssvu::lo() << "Socket bound correctly to port " + ssvu::toStr(port) + "\n";
+				selectClient();
 			}
+
+			session.tryBindSocket();
+			session.startSocketFuture();
 		}
 };
 
@@ -166,7 +447,9 @@ class Session
 int main()
 {	
 	Session s;
-	s.start();
+	ConsoleSessionController cs{s};
+
+	cs.start();
 
 	
 
@@ -199,6 +482,8 @@ int main()
 	ssvu::lo() << h1c->toJsonAll() << std::endl;
 
 	//ssvu::lo() << sizeof h1->x << std::endl;
+
+	s.join();
 
 	return 0;
 }
